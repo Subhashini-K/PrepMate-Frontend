b33c53e2c2d960f605c87e05dcfaed4ed95c92d7
Subhashini <subhashinikumar2004@gmail.com>
2025-06-27 08:23:32 +0530

    installed required libraries & defined app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import LandingPage from './pages/LandingPage';
+import Dashboard from "./pages/Home/Dashboard";
+import PrepMate from "./pages/PrepMate/PrepMate"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<LandingPage/>} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/prepmate/sessionId" element={<PrepMate/>} /> 
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+        />
+    </div>
   )
 }
 
