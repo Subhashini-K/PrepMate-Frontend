eb900a28360da4958aa9fe8aa105db8b333fd4e2
Subhashini <subhashinikumar2004@gmail.com>
2025-06-30 01:07:40 +0530

    integrated login API

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+
 
 
 const Login = ({setCurrentPage}) => {
@@ -29,7 +32,17 @@ const Login = ({setCurrentPage}) => {
 
     //Login API Call
     try{
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
 
+      const { token } = response.data;
+
+      if(token){
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
     }catch(error){
       if(error.response && error.response.data.messasge){
         setError(error.response.data.messasge);
